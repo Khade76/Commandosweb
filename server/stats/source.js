780b87caf810fd76798532706b73e3d3cb096881
Khade76 <darrananderson76@gmail.com>
2026-09-11 19:42:04 +0100

    Allow source endpoint time to query all servers

--- server/stats/source.js
+++ server/stats/source.js
@@ -17,7 +17,7 @@ export async function fetchStatsSource() {
   }
 
   const controller = new AbortController()
-  const timeout = setTimeout(() => controller.abort(), 15_000)
+  const timeout = setTimeout(() => controller.abort(), 30_000)
 
   try {
     const response = await fetch(url, {

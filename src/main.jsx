ce8be61fff5912cc1e8e1d96c4358e53dab07c9d
Khade76 <darrananderson76@gmail.com>
2026-09-11 12:01:17 +0100

    Load player stats styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './styles-routed.css'
+import './stats.css'
 
 class AppErrorBoundary extends Component {
   constructor(props) {

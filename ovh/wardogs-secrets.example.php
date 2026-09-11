ed14614e5d586360d0ec6a1aa1963622c9ae357c
Khade76 <darrananderson76@gmail.com>
2026-09-11 11:22:10 +0100

    Add OVH WARDOGS secret config example

---
+++ ovh/wardogs-secrets.example.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    // Keep this file outside the public www directory on OVH.
+    'cache_seconds' => 15,
+    'region' => 'Europe / UK',
+    'include_training_server' => true,
+
+    'servers' => [
+        [
+            'id' => 'wardogs-278c7bc5',
+            'name' => '44th Commandos #1',
+            'url' => 'http://165.217.136.52:9001',
+            'password' => 'CHANGE_ME_SERVER_1_RCON_PASSWORD',
+            'joinId' => '175590',
+            'maxPlayers' => 100,
+        ],
+        [
+            'id' => 'wardogs-9290beb1',
+            'name' => '44th Commandos #2',
+            'url' => 'http://165.217.136.99:9006',
+            'password' => 'CHANGE_ME_SERVER_2_RCON_PASSWORD',
+            'joinId' => '294832',
+            'maxPlayers' => 100,
+        ],
+    ],
+];
